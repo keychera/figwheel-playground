f8515e8af8126ceba21fd0df68dcae3d7e1319df
keychera <kyogatama@gmail.com>
2022-11-19 13:44:12 +0700

    init reagent

--- README.md
+++ README.md
@@ -2,12 +2,25 @@
 
 quick start
 
-    clojure -m figwheel.main --build dev --repl
+    clojure -A:fig:build
 
-learning resources used
+production
+
+    clojure -A:fig:min
+
+To clean all compiled files:
+
+    rm -rf target/public
+
+### learning resources used
 
 - https://clojurescript.org/guides/quick-start
 - https://figwheel.org/docs/installation.html
 - https://dawranliou.com/blog/vanilla-cljs/
 - https://github.com/kuzmisin/closurecheatsheet/blob/master/dom.md
 - https://google.github.io/closure-library/api/goog.dom.html
+
+### structure learnt from 
+this project is not generated, mainly just copy paste things that I need from the generated project
+
+    clj -Tclj-new create :template figwheel-main :name chera/hello :args '["--reagent"]'
--- src/chera/core.cljs
+++ src/chera/core.cljs
@@ -1,10 +1,28 @@
-(ns chera.core 
-  (:require [goog.dom :as gom]))
+(ns ^:figwheel-hooks chera.core 
+  (:require
+   [goog.dom :as gdom] 
+   [reagent.dom :as rdom]))
 
-(js/console.log "Chera says: there world!")
+(js/console.log "Chera says: Hello world!")
 
-(def adiv (gom/createDom "div" {:class "hey"} "chera is here"))
+(defn hello-world []
+  [:div
+   [:h1 "This is chera"]
+   [:h3 "and everything goes on..."]])
 
-(as-> (gom/getElement "app") it
-    (gom/append it adiv)
-    )
+; below seems to be the heart of reagent, the edge of the software
+
+(defn get-app-element []
+  (gdom/getElement "app"))
+
+(defn mount [el]
+  (rdom/render [hello-world] el))
+
+(defn mount-app-element []
+  (when-let [el (get-app-element)]
+    (mount el)))
+
+(mount-app-element)
+
+(defn ^:after-load on-reload []
+  (mount-app-element) )
