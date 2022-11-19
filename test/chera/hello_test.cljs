b4f0a6da97162bf2746475464f80f5c62da1b0fe
keychera <kyogatama@gmail.com>
2022-11-19 13:44:13 +0700

    setup auto-testing env

---
+++ test/chera/hello_test.cljs
@@ -0,0 +1,11 @@
+(ns chera.hello-test
+    (:require
+     [cljs.test :refer-macros [deftest is testing]]))
+
+(defn multiply [a b] 2)
+
+(deftest multiply-test
+  (is (= (* 1 2) (multiply 1 2))))
+
+(deftest multiply-test-2
+  (is (= (* 75 10) (multiply 10 75))))
