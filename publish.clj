41e26451d0a6528efa981dea8fdf26947a8922e8
keychera <kyogatama@gmail.com>
2022-12-20 15:54:37 +0700

    fix publish.clj

--- publish.clj
+++ publish.clj
@@ -1,6 +1,6 @@
 #!/usr/bin/env bb
 
-(require 
+(require
  '[clojure.main :refer [demunge]]
  '[babashka.fs :as fs]
  '[babashka.process :refer [shell]])
@@ -9,26 +9,22 @@
   (-> a-fn str demunge (str/split #"@") first (str/replace #"babashka." "")))
 
 (defn does [act & args]
-  (doall [(println (str "[bb] " (fn-name act) " " (reduce #(str %1 " " %2) args)))
-          (apply act args)]))
+  (println (str "[bb] " (fn-name act) " " (reduce #(str %1 " " %2) args)))
+  (apply act args))
 
 (defn clean-build []
-  (doall [(does fs/delete-tree "target/public")
-          (does shell "clojure" "-M:fig:min")
-          (does fs/delete-tree "target/public/cljs-out/dev")
-          (does fs/copy-tree "resources/public" "target/public")
-          (does fs/delete "target/public/css/.gitignore")
-]))
+  (does fs/delete-tree "target/public")
+  (does shell "clojure" "-M:fig:min")
+  (does fs/delete-tree "target/public/cljs-out/dev")
+  (does fs/copy-tree "resources/public" "target/public")
+  (does fs/delete "target/public/css/.gitignore"))
 
-;; error on push, "fatal: protocol '"https' is not supported" it said 
-;; (defn publish-target []
-;;   (doall [
-;;           (does shell {:dir "target/public"} "git" "init")
-;;           (does shell {:dir "target/public"} "git" "add" ".")
-;;           (does shell {:dir "target/public"} "git" "commit" "-m" "\"Deploy to GitHub Pages\"")
-;;           (does shell {:dir "target/public"} "git" "push" "--force" "\"https://github.com/keychera/figwheel-playground.git\"" "master:gh-pages") 
-;;           ]))
+(defn publish-target []
+  (does shell {:dir "target/public"} "git" "init")
+  (does shell {:dir "target/public"} "git" "add" ".")
+  (does shell {:dir "target/public" :continue true} "git" "commit" "-m" "\"Deploy to GitHub Pages\"")
+  (does shell {:dir "target/public"} "git" "push" "--force" "https://github.com/keychera/figwheel-playground.git" "main:gh-pages"))
 
 (clean-build)
-;; (publish-target)
+(publish-target)
 (prn "done!")
